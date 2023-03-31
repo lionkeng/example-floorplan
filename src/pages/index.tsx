9468d010962cda2ea79c9c13db53fe63767e6272
Keng Lim <keng@anewgo.com>
2023-03-31 12:14:21 -0400

    add interactive exterior

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import { SVGProps } from 'react'
+import { Dispatch, Fragment, SVGProps, SetStateAction, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -97,6 +99,72 @@ const footerNavigation = {
   ],
 }
 
+interface SlidingPanelI {
+  open: boolean
+  setOpen: Dispatch<SetStateAction<boolean>>
+}
+
+function SlidingPanel({ open, setOpen }: SlidingPanelI): JSX.Element {
+  let maxWidth = document.getElementById('plan-content')?.offsetWidth
+  if (maxWidth) {
+    maxWidth += 96 // padding on the right
+  }
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className={styles['design-dialog']} onClose={setOpen}>
+        <div className="fixed inset-0" />
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel
+                  className="pointer-events-auto w-screen"
+                  style={{ maxWidth }}
+                >
+                  <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                    <div className="px-4 sm:px-6">
+                      <div className="flex items-start justify-between">
+                        <Dialog.Title className={styles['floorplan-title']}>
+                          Design Exterior
+                        </Dialog.Title>
+                        <div className="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            onClick={() => setOpen(false)}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                      <iframe
+                        className={styles.floorplan}
+                        src="https://gazebo.anewgo.com/interactive-exterior/client/bakerresidential/community/5/plan/3/elev/10"
+                      />
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
+
 function Topper(): JSX.Element {
   return (
     <div className={styles.topper}>
@@ -143,10 +211,22 @@ function Sidebar(): JSX.Element {
   )
 }
 
-function PlanContent(): JSX.Element {
+interface PlanContentI {
+  handleDesignClick: () => void
+}
+
+function PlanContent({ handleDesignClick }: PlanContentI): JSX.Element {
   return (
-    <div className={styles['plan-content']}>
-      <div className={styles['plan-hero']} />
+    <div className={styles['plan-content']} id={'plan-content'}>
+      <div className={styles['plan-hero']}>
+        <div
+          role={'button'}
+          className={styles['plan-design-button']}
+          onClick={handleDesignClick}
+        >
+          Design this plan
+        </div>
+      </div>
       <div className={styles['plan-description']}>
         <div className={styles['first-paragraph']}>
           <span className={styles.emphasize}>D</span>
@@ -308,6 +388,10 @@ function Footer(): JSX.Element {
 }
 
 export default function Home() {
+  const [open, setOpen] = useState<boolean>(false)
+  const handleDesignClick = () => {
+    setOpen(!open)
+  }
   return (
     <>
       <Head>
@@ -323,10 +407,11 @@ export default function Home() {
         <Topper />
         <div className={styles['grid-container']}>
           <Sidebar />
-          <PlanContent />
+          <PlanContent handleDesignClick={handleDesignClick} />
         </div>
         <Footer />
       </main>
+      <SlidingPanel open={open} setOpen={setOpen} />
     </>
   )
 }
